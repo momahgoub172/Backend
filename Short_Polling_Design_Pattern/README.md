98724370bb6fbd0f02bfe8bad7bdc110e05129b9
Mohamed  Mahgoub <85798241+momahgoub172@users.noreply.github.com>
2023-10-19 08:18:27 -0700

    Short Polling Design Pattern

---
+++ Short_Polling_Design_Pattern/README.md
@@ -0,0 +1,5 @@
+# Project: Short Polling
+
+## Description
+- This project showcases a Web API server that utilizes short polling to provide near-real-time updates to clients. 
+- Short polling is a communication design pattern in which clients regularly send requests to check for updates, allowing the server to provide immediate updates when new data becomes available.
---
+++ Short_Polling_Design_Pattern/Short_Polling_Design_Pattern/Controllers/DemoController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Short_Polling_Design_Pattern.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        private static readonly IDictionary<Guid, int> Jobs = new Dictionary<Guid, int>();
+
+
+        [HttpPost("submit")]
+        public IActionResult SubmitJob()
+        {
+            var jobId = Guid.NewGuid();
+            Jobs[jobId] = 0;
+            //Jobs.Add(jobId, 0);
+            _ = UpdateJobAsync(jobId, 0);
+
+            return Ok(jobId);
+        }
+
+        [HttpGet("checkjob")]
+        public IActionResult CheckJob(Guid jobId)
+        {
+            // Check if the job exists
+            if (Jobs.ContainsKey(jobId))
+            {
+                return Ok(Jobs[jobId]);
+            }
+            return NotFound();
+        }
+
+
+        private async Task UpdateJobAsync(Guid jobId, int progress)
+        {
+            while (true)
+            {
+                if (progress == 100)
+                    return;
+
+                // Simulate progress update
+                progress += 10;
+                Jobs[jobId] = progress;
+
+                // Introduce an asynchronous delay without blocking the thread
+                await Task.Delay(2000);
+            }
+        }
+    }
+}
